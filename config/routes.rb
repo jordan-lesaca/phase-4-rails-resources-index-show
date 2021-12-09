d3e8c8800d003be7a99f46ea429f1246f26b4519
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-12-08 20:46:33 -0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
+#3=> One way to create routes 
+# Rails.application.routes.draw do
+#   get '/birds', to: 'birds#index' #=> Show a list of all birds
+#   get '/birds/:id', to: 'birds#show'#=> Show one specific bird
+# end 
+
+#3=> Another way to create routes. this will include all the routes
+# Rails.application.routes.draw do
+#   resources :birds
+# end 
+
+#3=> Another way to create routes. This will only include the the attributes to the right of ONLY
+#3=> Running rails routes now will give us the same output as when we wrote out the routes by hand
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :birds, only: [:index, :show]
+end 
+
+#4=> Handling the Index and Show Actions
+#4=> Set up controller:
+#4=> rails g controller Birds --no-test-framework
+#4=> go into the new BirdsController file. 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bird.create!(name: "Black-Capped Chickadee", species: "Poecile Atricapillus")
+Bird.create!(name: "Grackle", species: "Quiscalus Quiscula")
+Bird.create!(name: "Common Starling", species: "Sturnus Vulgaris")
+Bird.create!(name: "Mourning Dove", species: "Zenaida Macroura")
+
+#1=> Setup. Make a Bird model and generate some sample data for our API. 
+#1=> Use Rails Generator to set up our model:
+#1=> rails g model Bird name species --no-test-framework
+#1=> After generating the model, run the migration:
+#1=> rails db:migrate
+
+#2=> Next, create seed data in the db/seeds.rb file. 
+#2=> To add these birds to our database, run the seed file:
+#2=> rails db:seed
+#2=> Next, go to route file to set up index and show actions
